fe61547eb169292c3ea507cfbd07e349c02cb2cf
rajan <rajan@simformsolutions.com>
2022-04-22 19:07:20 +0530

    life-cycle-hooks

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,14 +1,26 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css'],
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent implements OnInit, OnChanges {
   @Input() products: { name: string; price: number }[];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnchanges called of product component');
+    console.log(changes);
+  }
+  ngOnInit(): void {
+    console.log('ngOnInit called of product component');
+  }
 }
